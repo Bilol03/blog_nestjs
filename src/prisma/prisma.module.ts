13871e161614ed2088d98409971993dd51fab92a
Bilol <madiyarovbilol78@gmail.com>
2025-04-06 15:16:04 +0500

    prisma service has been finished

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,7 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
+@Global() // makes PrismaService available app-wide without importing each time
 @Module({
-  providers: [PrismaService]
+  providers: [PrismaService],
+  exports: [PrismaService],
 })
 export class PrismaModule {}
